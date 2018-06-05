6b338f063f9c1c954b1da8ea2f3b63aa53f2d38b
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-06-05 22:47:24 +0530

    Update List of Lists.py

--- List of Lists.py
+++ List of Lists.py
@@ -6,21 +6,23 @@
               ['Stanford',19535,40569],
               ['Yale',11701,40500]  ]
 
-def total_enrollment(a):                     # Defining a procedure
-	i = []                                   # Defining variable having empty list for storing total number of students
-	j = []                                   # Defining variable having empty list for storing total number of fees (fees of individual university * number of students enrolled)
-	final = []                               # Defining variable having empty list which will store final value (["total number of srudents", "total fees"])
-	for x in a:                              # For x (individual value of a)
-		#print (x[1])                                  # you can un-comment print to see what's happening in the block
-		i.append(x[1])                       # Appending i[] with every x[1] value ,i.e, [2175, 19627, 10566, ...., ]
-	for y in a:                              # For y (individual value of a)
-		y = y[1] * y[2]                      # As given, total number of fees (fees of individual university * number of students enrolled) so y = y[1] * y[2]
-		#print (y)                                     # you can un-comment print to see what's happening in the block
-		j.append(y)                          # Appending j[] with every y value
-	sum_i = sum(i)                           # sum_i will sum the values in i
-	sum_j = sum(j)                           # sum_j will sum the values in j
-	final.append(sum_i)                      # Appending sum_i and sum_j to final[]
+def total_enrollment(a):           # Defining a procedure
+	i = []                     # Defining variable having empty list for storing total number of students
+	j = [                      # Defining variable having empty list for storing total number of fees (fees of individual university * number of students enrolled)
+	final = []                 # Defining variable having empty list which will store final value (["total number of srudents", "total fees"])
+	for x in a:                # For x (individual value of a)
+		#print (x[1])          # you can un-comment print to see what's happening in the block
+		i.append(x[1])     # Appending i[] with every x[1] value ,i.e, [2175, 19627, 10566, ...., ]
+	for y in a:                # For y (individual value of a)
+		y = y[1] * y[2]    # As given, total number of fees (fees of individual university * number of students enrolled) so y = y[1] * y[2]
+		#print (y)             # you can un-comment print to see what's happening in the block
+		j.append(y)        # Appending j[] with every y value
+	sum_i = sum(i)             # sum_i will sum the values in i
+	sum_j = sum(j)             # sum_j will sum the values in j
+	final.append(sum_i)        # Appending sum_i and sum_j to final[]
 	final.append(sum_j)
-	return final                             # Returning final
+	return final               # Returning final
 	
-print (total_enrollment(usa_univs))	
+print (total_enrollment(usa_univs))
+		
+# CODED BY - GAURAV PADAWE
